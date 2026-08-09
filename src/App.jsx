aeeaef4f0c8248e751352b70b44f180b0ddd9216
abdullahalmsaodi <abdullah.almsaodi@gmail.com>
2026-08-09 15:04:17 +0300

    refactor: clean up HTML and improve App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import { Chatbot } from 'supersimpledev';
+import { Chatbot } from "supersimpledev";
 import { ChatInput } from "./components/ChatInput";
 import { ChatMessages } from "./components/ChatMessages";
+import robotIcon from "./assets/robot.png";
 
 import "./App.css";
 
 function App() {
   const [chatMessages, setChatMessages] = useState([]);
-
 
   useEffect(() => {
     // console.log("Running effect");
@@ -19,23 +19,26 @@ function App() {
     // console.log(Chatbot.additionalResponses);
   }, []);
 
-
-
   return (
-    <div className="app-container">
-      {chatMessages.length === 0 ? (
-        <p className="welcome-message">
-          Welcome to the chatbot project! Send a message using the textbox
-          below.
-        </p>
-      ) : (
-        <ChatMessages chatMessages={chatMessages} />
-      )}
-      <ChatInput
-        chatMessages={chatMessages}
-        setChatMessages={setChatMessages}
-      />
-    </div>
+    <>
+      <link rel="icon" type="image/svg+xml" href={robotIcon} />
+      <title>Chatbot Project</title>
+
+      <div className="app-container">
+        {chatMessages.length === 0 ? (
+          <p className="welcome-message">
+            Welcome to the chatbot project! Send a message using the textbox
+            below.
+          </p>
+        ) : (
+          <ChatMessages chatMessages={chatMessages} />
+        )}
+        <ChatInput
+          chatMessages={chatMessages}
+          setChatMessages={setChatMessages}
+        />
+      </div>
+    </>
   );
 }
 
